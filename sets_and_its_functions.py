70fc945af1cec022b07890b69aa1d5276d0e6635
SubratoScouTOP <subratoclass10@gmail.com>
2025-06-26 16:07:51 +0530

    Add files via upload

---
+++ sets_and_its_functions.py
@@ -0,0 +1,11 @@
+
+friends_list = ["Aman", "Priya", "Ravi", "Neha", "Karan","Simran", "Rohit", "Sneha", "Anjali", "Vikas","Aman", "Priya", "Meena", "Sahil", "Tina"]
+friends_set = set(friends_list)
+print("Friends set:")
+print(friends_set)
+friends_set.add("Deepak")
+print("\nAfter adding 'Deepak':")
+print(friends_set)
+friends_set.remove("Ravi")
+print("\nAfter removing 'Ravi':")
+
