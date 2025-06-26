7dfd6241d6a53ae42417df8ab3dceb32f1bdfe55
SubratoScouTOP <subratoclass10@gmail.com>
2025-06-26 14:38:07 +0530

    Add files via upload

---
+++ using_list_operations.py
@@ -0,0 +1,13 @@
+roll_numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+students_name=["shubham ", "shivansh","rupesh","amit","sumit","abhinav","asif","akul","shivam","ankush","abhijeet","ashutosh", "saurabh","saurav","bheem"]
+#print(list(roll_numbers),(students_name))
+roll_numbers.append(16)
+students_name.append("arjun")
+roll_numbers.extend([17])
+students_name.extend("rahul")
+roll_numbers.remove(2)
+students_name.remove("shivansh")
+roll_numbers.pop
+students_name.pop
+print(list(students_name))
+print(list(roll_numbers))
