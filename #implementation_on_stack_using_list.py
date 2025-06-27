8045b28aa7b540c8ef6bbf4a9640ab0c02c527ba
SubratoScouTOP <subratoclass10@gmail.com>
2025-06-27 10:40:16 +0530

    Delete #implementation_on_stack_using_list.py

--- #implementation_on_stack_using_list.py
+++
@@ -1,6 +0,0 @@
-stack = []  
-stack.append(["A","hello"])
-print("Stack after pushing:", stack)
-print("Top of the stack (peek):", stack[-1]) #as lifo
-print("Popped item:", stack.pop())
-print("Stack after popping:", stack)
