596792e33795154ec6b1f2e35236e25778e67769
SubratoScouTOP <subratoclass10@gmail.com>
2025-06-27 10:20:27 +0530

    Add files via upload

---
+++ implentation_queue_using_empty_list.py
@@ -0,0 +1,6 @@
+queue = []
+queue.append(["aditya","goodmorning"])
+print("Queue after pushing:", queue)
+print("Front of the queue (peek):", queue[0])#as fifo
+print("Popped item:", queue.pop(0))
+print("Queue after popping:", queue)
