9e7945eeebe4cfdb8600d9df5bc64dd833a4ce21
SubratoScouTOP <subratoclass10@gmail.com>
2025-06-27 10:39:57 +0530

    Delete implentation_queue_using_empty_list.py

--- implentation_queue_using_empty_list.py
+++
@@ -1,6 +0,0 @@
-queue = []
-queue.append(["aditya","goodmorning"])
-print("Queue after pushing:", queue)
-print("Front of the queue (peek):", queue[0])#as fifo
-print("Popped item:", queue.pop(0))
-print("Queue after popping:", queue)
